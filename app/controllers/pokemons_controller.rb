33faf953f9bfdf7aefe24aa62cd0945f38da5fa1
hama <hmmshl@gmail.com>
2023-02-27 17:11:44 +0100

    simple index added

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,6 @@
 class PokemonsController < ApplicationController
+  def index
+    @pokemons = Pokemon.all
+    @pokemons = @pokemons.where("name ILIKE '%#{params[:search]}%'") if params[:search].present?
+  end
 end
